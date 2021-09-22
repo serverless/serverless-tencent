3957934189cae7910c4d00ba1208dedc07decff5
mzong <11454175+zongUMR@users.noreply.github.com>
2021-09-22 17:45:19 +0800

    Refine structure and files

---
+++ src/index.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const config = require('./config');
+const Cli = require('./cli');
+const functions = require('./libs');
+const { loadTencentGlobalConfig } = require('./utils');
+
+const commandList = [
+  'init',
+  'deploy',
+  'remove',
+  'logs',
+  'dev',
+  'info',
+  'registry',
+  'publish',
+  'help',
+  'version',
+  'auto', // interatively init a project command
+  'custom',
+];
+
+class ServerlessTencentPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+    this.config = config();
+    this.cli = new Cli(this.config);
+
+    loadTencentGlobalConfig(this.cli, this.config);
+
+    this.commands = commandList.reduce(
+      (value, curr) => ({
+        [curr]: {
+          lifecycleEvents: ['exec'],
+        },
+        ...value,
+      }),
+      {
+        invoke: {
+          lifecycleEvents: ['exec'],
+          commands: {
+            local: {
+              lifecycleEvents: ['exec'],
+            },
+          },
+        },
+
+        credentials: {
+          commands: {
+            set: {
+              lifecycleEvents: ['exec'],
+            },
+            list: {
+              lifecycleEvents: ['exec'],
+            },
+            remove: {
+              lifecycleEvents: ['exec'],
+            },
+          },
+        },
+      }
+    );
+
+    this.hooks = commandList.reduce(
+      (value, curr) => ({
+        [`${curr}:exec`]: async () => await functions(this.config, this.cli),
+        ...value,
+      }),
+      {
+        'version:exec': () => {
+          this.cli.logVersion();
+        },
+        'invoke:exec': async () => await functions(this.config, this.cli),
+        'invoke:local:exec': async () => await functions(this.config, this.cli),
+        'credentials:set:exec': async () => await functions(this.config, this.cli),
+        'credentials:list:exec': async () => await functions(this.config, this.cli),
+        'credentials:remove:exec': async () => await functions(this.config, this.cli),
+      }
+    );
+  }
+}
+
+module.exports = ServerlessTencentPlugin;
---
+++ src/libs/invoke/invoke-local/index.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const path = require('path');
+const utils = require('../../utils');
+const { readAndParseSync, fileExistsSync } = require('../../../utils');
+const { colorLog, printOutput, summaryOptions, checkRuntime } = require('./utils');
+const runPython = require('./runPython');
+const runPhp = require('./runPhp');
+const { generatePayload, storeLocally } = require('../../telemtry');
+
+module.exports = async (config, cli, command, instanceDir) => {
+  const { config: ymlFilePath, c } = config;
+
+  let instanceYml = await utils.loadInstanceConfig(instanceDir, command);
+  const telemtryData = await generatePayload({
+    command: 'invoke_local',
+    rootConfig: instanceYml,
+  });
+
+  try {
+    if (ymlFilePath || c) {
+      const customizedConfigFile = ymlFilePath || c;
+
+      if (!fileExistsSync(path.join(instanceDir, customizedConfigFile))) {
+        await storeLocally({
+          ...telemtryData,
+          outcome: 'failure',
+          failure_reason: '指定的yml文件不存在',
+        });
+        throw new Error('指定的yml文件不存在');
+      }
+      instanceYml = readAndParseSync(customizedConfigFile);
+    }
+
+    const { inputs = {}, component } = instanceYml;
+
+    // Currently we only support local invoke for scf component
+    if (!component.includes('scf')) {
+      await storeLocally({
+        ...telemtryData,
+        outcome: 'failure',
+        failure_reason: '当前命令只支持 SCF 组件，请在 SCF 组件目录内使用',
+      });
+      colorLog('当前命令只支持 SCF 组件，请在 SCF 组件目录内使用', 'yellow', cli);
+    }
+
+    try {
+      const [eventData, contextData, handlerFile, handlerFunc] = summaryOptions(
+        config,
+        instanceYml,
+        cli
+      );
+
+      const runtime = inputs.runtime;
+      checkRuntime(runtime, cli);
+
+      if (runtime.includes('Nodejs')) {
+        const invokeFromFile = path.join(instanceDir, handlerFile);
+        const exportedVars = require(`${invokeFromFile}`);
+        const finalInvokedFunc = exportedVars[handlerFunc];
+        if (!finalInvokedFunc) {
+          colorLog(`调用的函数 ${handlerFunc} 不存在， 请检查后重试。`, 'yellow', cli);
+        }
+        try {
+          const result = await finalInvokedFunc(eventData, contextData);
+          printOutput(cli, result);
+        } catch (e) {
+          printOutput(cli, null, e);
+        }
+      }
+
+      if (runtime.includes('Python')) {
+        await runPython(eventData, contextData, handlerFile, handlerFunc, cli);
+      }
+
+      if (runtime.includes('Php')) {
+        await runPhp(eventData, contextData, handlerFile, handlerFunc, cli);
+      }
+    } catch (e) {
+      await storeLocally({
+        ...telemtryData,
+        outcome: 'failure',
+        failure_reason: e.message,
+      });
+      throw e;
+    }
+
+    await storeLocally({ ...telemtryData, outcome: 'success' });
+  } catch (e) {
+    await storeLocally({
+      ...telemtryData,
+      outcome: 'failure',
+      failure_reason: e.message,
+    });
+    throw e;
+  }
+};
